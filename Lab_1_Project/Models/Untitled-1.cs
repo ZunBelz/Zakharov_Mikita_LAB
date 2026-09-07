942ded9dae06b5d3ffe1a6546ad1add7a834875b
ZunBelz <kn1b25.zakharov@kpnu.edu.ua>
2026-09-07 09:29:15 +0300

    Add files via upload

---
+++ Lab_1_Project/Models/Untitled-1.cs
@@ -0,0 +1,10 @@
+// Product.cs
+
+namespace ShopApp.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
